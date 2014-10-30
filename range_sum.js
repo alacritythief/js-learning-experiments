d028d0f56822403748b071195a307944621c879d
Andy Wong <alacritythief@gmail.com>
2014-10-30 14:14:25 -0400

    Added range_sum.js

---
+++ range_sum.js
@@ -0,0 +1,27 @@
+/*
+RANGE & SUM
+Creating a function that gives an array from a number range,
+and a function that sums up an array.
+*/
+
+function range(x, y) {
+  var array = [];
+  for (x; x <= y; x++) {
+    array.push(x);
+  }
+  return array;
+}
+
+console.log("Array of 1 - 10: ");
+console.log(range(1, 10));
+
+function sum(array) {
+  var total = 0;
+  for (var count = 0; count <= array.length - 1; count++) {
+    total += array[count];
+  }
+  return total;
+}
+
+console.log("Sum of array 1 - 10: ");
+console.log(sum(range(1, 10)));
